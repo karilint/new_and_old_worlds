39f2fb5201eba387940354069ab82bc55153ceaa
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2025-08-04 15:59:59 +0300

    Enable publication import/export in admin

--- now/admin.py
+++ now/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Publication
 
 
+class PublicationResource(resources.ModelResource):
+    class Meta:
+        model = Publication
+        fields = (
+            "id",
+            "authors",
+            "title",
+            "journal",
+            "citation",
+            "year",
+            "doi",
+        )
+
+
 @admin.register(Publication)
-class PublicationAdmin(admin.ModelAdmin):
+class PublicationAdmin(ImportExportModelAdmin):
+    resource_class = PublicationResource
     list_display = ("year", "title", "authors")
     list_filter = ("year",)
     search_fields = ("title", "authors", "journal", "doi")
