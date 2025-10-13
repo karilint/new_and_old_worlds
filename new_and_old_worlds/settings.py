54e3f3feb61fbf2c18f45fd76796001e077be9d4
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2025-10-13 15:02:16 +0300

    whitenoise>=6.6.0

--- new_and_old_worlds/settings.py
+++ new_and_old_worlds/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
     'import_export',
     'now',
